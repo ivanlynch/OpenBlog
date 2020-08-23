7153b58227efb83e72af2f5c50ae628a20f0bc4f
Iván Lynch <ivan.lynch@outlook.com.ar>
2020-08-23 19:40:19 -0300

    Added Formik

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -1,20 +1,78 @@
-import {SafeAreaView, Text, View, Button} from 'react-native';
+import {TextInput, View, Button, StyleSheet} from 'react-native';
+import React from 'react';
+import {Formik} from 'formik';
 
-import React, {useState} from 'react';
-
-export const NewPostForm = (props) => {
-  const [state, setState] = useState({nombre: ''});
+/**
+ * @param {{ onSubmit: () => void; }} props
+ */
+export default function NewPostForm(props) {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>{state.nombre}</Text>
-        <Button
-          title="Soy un boton"
-          onPress={() => {
-            setState({nombre: 'Ivan'});
-          }}
-        />
-      </View>
-    </SafeAreaView>
+    <View style={styles.formContainer}>
+      <Formik
+        initialValues={{title: '', body: '', author: ''}}
+        onSubmit={(values, actions) => {
+          console.log(values);
+          actions.resetForm();
+          props.onSubmit();
+        }}>
+        {(formikprops) => (
+          <View style={styles.formGroup}>
+            <TextInput
+              key="title"
+              style={styles.formInput}
+              placeholder="insert title"
+              onChangeText={formikprops.handleChange('title')}
+              value={formikprops.values.title}
+            />
+            <TextInput
+              key="body"
+              multiline
+              style={styles.formInput}
+              placeholder="insert body"
+              onChangeText={formikprops.handleChange('body')}
+              value={formikprops.values.body}
+            />
+            <TextInput
+              key="author"
+              style={styles.formInput}
+              placeholder="insert author"
+              onChangeText={formikprops.handleChange('author')}
+              value={formikprops.values.author}
+            />
+            <Button
+              title="Submit"
+              color="maroon"
+              // @ts-ignore
+              onPress={formikprops.handleSubmit}
+            />
+          </View>
+        )}
+      </Formik>
+    </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  formContainer: {
+    height: '40%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    width: '100%',
+  },
+  formInput: {
+    borderWidth: 1,
+    width: '100%',
+    borderColor: '#FFDAC1',
+    padding: 10,
+    fontSize: 18,
+    margin: 2,
+    borderRadius: 5,
+  },
+  formGroup: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
+  },
+});
--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,15 +1,17 @@
 // @ts-nocheck
 import React, {useState} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Modal} from 'react-native';
 import {TouchableHighlight} from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import ModalMenu from './ModalMenu';
+import NewPostForm from '../components/NewPostForm';
 
 /**
  * @param {{ style: boolean | import("react-native").TextStyle | import("react-native").RegisteredStyle<import("react-native").TextStyle> | import("react-native").RecursiveArray<false | import("react-native").TextStyle | import("react-native").RegisteredStyle<...>>; title: React.ReactNode; }} props
  */
 export default function Header(props) {
   const [isOpen, setOpen] = useState(false);
+  const [isFormVisible, setFormVisible] = useState(false);
   return (
     <View style={styles.headerContainer}>
       <View>
@@ -17,11 +19,28 @@ export default function Header(props) {
       </View>
       <ModalMenu
         visible={isOpen}
-        options={[{title: 'Add Post', onPress: () => {}}]}
+        options={[
+          {
+            title: 'Add Post',
+            onPress: () => {
+              setOpen(false);
+              setFormVisible(true);
+            },
+          },
+        ]}
         onCancel={() => {
           setOpen(false);
         }}
       />
+      <Modal visible={isFormVisible} animationType="fade" transparent={true}>
+        <View style={styles.formContainer}>
+          <NewPostForm
+            onSubmit={() => {
+              setFormVisible(false);
+            }}
+          />
+        </View>
+      </Modal>
       <View style={styles.buttonWrapper}>
         <TouchableHighlight
           underlayColor="rgba(73,182,77,1,0.9)"
@@ -60,9 +79,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     paddingRight: 10,
   },
-
   modalOpacity: {
     flex: 1,
+    backgroundColor: 'rgba(100,100,100, 0.5)',
+  },
+  formContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: 'rgba(100,100,100, 0.5)',
   },
 });
